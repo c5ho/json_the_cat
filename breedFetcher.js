05ca44d198311ec9ae124cc10c7438ca9300477e
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-09 19:43:29 -0400

    added testing for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,16 +6,16 @@ const fetchBreedDescription = function(breedName, callback) {
   
   request(URL, (error, response, body) => {
     if (error) {
-      return callback(error, null); //returns the error via callback if one occurred, since there isn't a second paramater, we can leave it blank or put null
+      return callback(error); //returns the error via callback if one occurred, since there isn't a second paramater, we can leave it blank or put null
     }
     
     const foundBreed = JSON.parse(body);  //foundBreed is the cat object(s) of the searched cat breed
 
     if (foundBreed.length === 0) {
-      return callback(null, 'This breed was not found in this database.');
+      return callback('This breed was not found in this database.');
     }
     //if search string is not specific enough and multiple search results are returned, it will only return description of the first breed that satisfies the string (eg. American -> American Bobtail instead of American Curl)
-    return callback(null, foundBreed[0].description);
+    return callback(null, foundBreed[0].description); //need null here since there is no error, but must pass the desciption as the second parameter, otherwise it would be recognized as the first 'error' parameter
   });
 };
 
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const searchBreed =  process.argv.slice(2).join(' ');
 
-fetchBreedDescription(searchBreed, (error, description) => {
+fetchBreedDescription(searchBreed, (error, description) => { //the fn call for fetchBreed Description, which also includes a callback fn inline with (error, description) parameters
+  
   if (error) {
     console.log('Error fetch details:', error);
   } else {
